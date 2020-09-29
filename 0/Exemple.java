d1819ae69e074816a83b76ad4cd207db83ff4f3b
bapateyron <bastien.pateyron@etu.isima.fr>
2020-09-29 09:53:22 +0200

    🔖 Exercice 0

---
+++ 0/Exemple.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+public class Exemple {
+   public static void main(String[] args) {
+      // foreach
+      for (String arg : args) {
+         System.out.println(arg);;
+      }
+
+      String s = "";
+      for (int i = 0; i < 65536; i++) {
+         s += "coucou";
+      }
+   }
+}
+
