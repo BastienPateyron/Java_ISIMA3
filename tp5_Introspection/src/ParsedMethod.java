4508aaf54e5a27a65668acad613ee6e408c6e022
bapateyron <bastien.pateyron@etu.isima.fr>
2020-11-09 18:18:15 +0100

    TP5: Parsing params HPP ok

--- tp5_Introspection/src/ParsedMethod.java
+++ tp5_Introspection/src/ParsedMethod.java
@@ -1,24 +1,24 @@
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
 
 public class ParsedMethod extends ClassParser {
    public String params = "";
+   public ArrayList<String> param_list = new ArrayList<>();
 
    public ParsedMethod(Method method) {
       super(method);
       type = method.getReturnType().getSimpleName();
-      for (Class p : method.getParameterTypes())
-         params += p.getSimpleName() + ", ";
-      if (params.equals("") == false)
-         params = params.substring(0, params.length() - 2);
+      for (Class p : method.getParameterTypes()) {
+         param_list.add(p.getSimpleName());
+      }      
    }
 
    public ParsedMethod(Constructor constructor) {
       super(constructor);
-      for (Class p : constructor.getParameterTypes())
-         params += p.getSimpleName() + ", ";
-      if (params.equals("") == false)
-         params = params.substring(0, params.length() - 2);
+      for (Class p : constructor.getParameterTypes()) {
+         param_list.add(p.getSimpleName());
+      }
    }
 
    // public String toString() {
@@ -30,8 +30,16 @@ public class ParsedMethod extends ClassParser {
    public String toHpp() {
       StringBuilder s = new StringBuilder();
       s.append(type).append(' ')
-       .append(name).append('(')
-       .append(params).append(");");
+       .append(name).append('(');
+      
+      for(int i = 0; i < param_list.size(); i++) {
+         s.append(param_list.get(i));
+         if(i < param_list.size() -1) {
+            s.append(", ");
+         }
+      }
+
+      s.append(");");
 
       return s.toString().trim();
    }
@@ -45,6 +53,8 @@ public class ParsedMethod extends ClassParser {
        
        // TODO: Modifier params dans le Cpp pour qu'il ajoute des noms aux arguments
        // TODO: Rajouter les parenthses et un return du type souhaité
+
+
 
 
       return s.toString().trim();
